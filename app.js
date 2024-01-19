5dab6a42fce3508e9725543a632e12dbebd72c67
minhnq <minhnguyen31223@gmail.com>
2024-01-19 10:09:29 +0700

    setup signIn

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const express = require("express");
 const app = express();
 
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
+const signInRouter = require("./src/routers/signIn");
+app.use("/", signInRouter);
+
 app.get("/", (req, res) => {
-  res.send("Chào mừng đến với website của Leader Anh Khoa");
+  res.redirect("/signIn");
 });
+// app.get("/signIn", (req, res) => {
+//   const data = { username: "John" };
+//   res.render("signIn", data);
+// });
+
+// app.get("/home", (req, res) => {
+//   const data = { username: "John" };
+//   res.render("home", data);
+// });
 
 const PORT = 3000;
 app.listen(PORT, () => {
