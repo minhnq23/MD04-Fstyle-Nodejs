1a23828ee8ed13e1f7034aba7f51aa106a60505a
Minh Nguyễn <94302573+minhnq23@users.noreply.github.com>
2024-01-25 15:48:08 +0700

    Update app.js

--- app.js
+++ app.js
@@ -2,34 +2,23 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const user = require("./models/user");
-
+const signInRouter = require("./src/routers/signIn");
 app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
-const signInRouter = require("./src/routers/signIn");
-const loginRouter = require("./src/routers/login");
+app.use("/", signInRouter);
 
-// app.use("/", signInRouter);
-app.use("/", loginRouter);
-app.use("/signIn", signInRouter);
 
-<<<<<<< HEAD
-app.get("/", (req, res) => {
-  res.redirect("/login");
-});
 
-app.get("/signIn", (req, res) => {
-=======
-////////////////////////////////////////////////////////////////
+
 const PORT = 3000;
 const uri =
   "mongodb+srv://minhnq23:minh31223@cluster.u3ap31e.mongodb.net/?retryWrites=true&w=majority";
 //
 app.get("/", async (req, res) => {
-  await mongoose.connect(uri).then(console.log("connect mongoDb thanh cong"));
->>>>>>> 1c599f6c9689c2d1a484a69445ac5869323c862c
   res.redirect("/signIn");
 });
 
-app.listen(PORT, () => {
+app.listen(PORT,async () => {
   console.log(`Server started on port ${PORT}`);
+    await mongoose.connect(uri).then(console.log("connect mongoDb thanh cong"));
 });
