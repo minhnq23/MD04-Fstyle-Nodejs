9f44af424b7fed82d00e7a4e9edd40484b6f34bb
Admin <nguyenkhacdung.bn00@gmail.com>
2024-02-21 09:44:22 +0700

    fix Api san pham

--- src/api/productApi.js
+++ src/api/productApi.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 const productController = require("../controllers/productController");
 
-router.get("/api/products/products", productController.getProduct);
-router.post("/api/products/products", productController.createProduct);
+router.get("/api/products/:id", productController.getProduct);
+router.post("/api/products/post", productController.createProduct);
 router.get("/api/products/update/:id", productController.updateProduct);
 
 module.exports = router;
