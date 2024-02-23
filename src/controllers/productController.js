08b738299f2a9e1401ca3a2f3da9efea2152c92a
Admin <nguyenkhacdung.bn00@gmail.com>
2024-02-23 15:08:47 +0700

    fix controllerproduct

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,45 +1,93 @@
 const express = require("express");
 const Product = require("../models/product");
 
-exports.getProduct = async (req, res) => {
-  const id = req.params.id;
-  let product = await Product.findById(id).lean();
-  res.json(product);
-};
-
 exports.createProduct = async (req, res) => {
+  const {
+    name,
+    image64,
+    brand,
+    price,
+    size,
+    color,
+    quantity,
+    type,
+    description,
+  } = req.body;
+
   const product = new Product({
-    name: req.body.name,
-    image64: req.body.image64,
-    brand: req.body.brand,
-    price: req.body.price,
-    size: req.body.size,
-    color: req.body.color,
-    quantity: req.body.quantity,
-    type: req.body.type,
-    description: req.body.description,
+    name,
+    image64,
+    brand,
+    price,
+    size,
+    color,
+    quantity,
+    type,
+    description,
   });
 
   try {
     const newProduct = await product.save();
-    res.status(201).json(newProduct);
+    res.status(201).json({
+      status: 201,
+      message: "Product saved successfully",
+      product: newProduct,
+    });
   } catch (error) {
-    res.status(400).json({ message: error.message });
+    res.status(400).json({ status: 400, message: error.message });
   }
 };
+
 exports.updateProduct = async (req, res) => {
-  const _id = req.params.id;
+  const productId = req.params.id;
   const updates = req.body;
 
   try {
-    const updatedProduct = await Product.findByIdAndUpdate(_id, updates, {
+    const updatedProduct = await Product.findByIdAndUpdate(productId, updates, {
       new: true,
     });
     if (!updatedProduct) {
-      return res.status(404).json({ message: "Không tìm thấy sản phẩm" });
+      return res
+        .status(404)
+        .json({ status: 404, message: "Không tìm thấy sản phẩm" });
     }
-    res.json(updatedProduct);
+    res.status(200).json({
+      status: 200,
+      message: "Sản phẩm cập nhật thành công",
+      product: updatedProduct,
+    });
   } catch (error) {
-    res.status(400).json({ message: error.message });
+    res.status(400).json({ status: 400, message: error.message });
+  }
+};
+
+exports.getProduct = async (req, res) => {
+  const productId = req.params.id;
+
+  try {
+    const product = await Product.findById(productId).lean();
+    if (!product) {
+      return res
+        .status(404)
+        .json({ status: 404, message: "Không tìm thấy sản phẩm" });
+    }
+    res.status(200).json({
+      status: 200,
+      product: product,
+    });
+  } catch (error) {
+    res.status(400).json({ status: 400, message: error.message });
+  }
+};
+
+exports.getAllProducts = async (req, res) => {
+  try {
+    const products = await Product.find().lean();
+    res.status(200).json({
+      status: 200,
+      products: products,
+    });
+  } catch (error) {
+    res.status(500).json({ status: 500, message: error.message });
   }
 };
