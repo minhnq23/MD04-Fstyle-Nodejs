03137e3a245f8c03fdac090b31c83a72a387594f
Admin <nguyenkhacdung.bn00@gmail.com>
2024-02-23 14:29:07 +0700

    fix api product

--- src/api/productApi.js
+++ src/api/productApi.js
@@ -2,8 +2,11 @@ const express = require("express");
 const router = express.Router();
 const productController = require("../controllers/productController");
 
-router.get("/api/products/:id", productController.getProduct);
+router.post("/api/products/:id", productController.getProduct);
 router.post("/api/products/post", productController.createProduct);
-router.get("/api/products/update/:id", productController.updateProduct);
+router.put(
+  "/api/products/update/:id/:id_product",
+  productController.updateProduct
+);
 
 module.exports = router;
