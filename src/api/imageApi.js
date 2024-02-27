f0e283eefaa426878183347bf2471bbdaf3b25e2
minhnq <minhnguyen31223@gmail.com>
2024-02-27 10:53:23 +0700

    api image user

--- src/api/imageApi.js
+++ src/api/imageApi.js
@@ -2,5 +2,5 @@ const express = require("express");
 const router = express.Router();
 const imageController = require("../controllers/imageController");
 
-router.post("/api/user/avatar/image/:id", imageController.imageUser);
+router.get("/api/user/avatar/image/:id", imageController.imageUser);
 module.exports = router;
