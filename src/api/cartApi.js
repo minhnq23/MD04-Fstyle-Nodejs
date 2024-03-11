70a41a8e04ce1ddf9654a252b8d6f458139c8cb1
minhnq <minhnguyen31223@gmail.com>
2024-03-11 15:34:46 +0700

    cart api add product

--- src/api/cartApi.js
+++ src/api/cartApi.js
@@ -1,2 +1,7 @@
+const express = require("express");
+const router = express.Router();
+const cartController = require("../controllers/cartController");
 
+router.put("/api/add/product/cart/:id_user", cartController.addCart);
 
+module.exports = router;
