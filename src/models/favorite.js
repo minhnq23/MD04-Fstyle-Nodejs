ea0989bd97fab8d6e5c31558114de7a1320f902d
Admin <nguyenkhacdung.bn00@gmail.com>
2024-03-12 14:35:25 +0700

    fix lan cuôi

--- src/models/favorite.js
+++ src/models/favorite.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 const FavoriteProductSchema = new mongoose.Schema({
   userId: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
     required: true,
+    unique: true,
   },
   listProduct: [
     {
@@ -18,6 +18,13 @@ const FavoriteProductSchema = new mongoose.Schema({
       quantity: {
         type: Number,
         default: 1,
+      },
+      price: {
+        type: Number,
+        default: 0.0,
+      },
+      image64: {
+        type: String,
       },
     },
   ],
